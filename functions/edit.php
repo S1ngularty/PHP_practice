4ce5c85bcb5c116feac00680bcbd55da414842fc
PoisedStatute39 <166142644+PoisedStatute39@users.noreply.github.com>
2024-10-22 23:47:37 +0800

    update profile interface 1.0

--- functions/edit.php
+++ functions/edit.php
@@ -2,6 +2,7 @@
 session_start();
 include ("../includes/config.php");
 include ("../structure/header.html");
+include ("../includes/framework.html");
 try{
 
 if(!empty($_SESSION['user_id'])){
@@ -25,58 +26,80 @@ on u.user_id=a.user_id inner join profile p on
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+  .main{
+    justify-content: space-evenly;
+    display: flex;
+    border: thin solid;
+    padding: 20px;
+width: 60%;
+  }
+
+  .form-label{
+    text-align: start;
+  }
+</style>
 <body>
 <div class="container">
     <form action="update.php" method="POST" enctype="multipart/form-data">
-    <div class="firstname">
-    <label for="">First Name:</label>
-    <input type="text" name="firstname" placeholder="user" value="<?php echo $row['first_name'] ?>">
+    <center>
+  <div class="main">
+  <div class="con1">
+   <div class="firstname">
+    <label class="form-label" for="">First Name:</label>
+    <input class="form-control" type="text" name="firstname" placeholder="user" value="<?php echo $row['first_name'] ?>">
   </div>
-
+<br>
   <div class="lastname">
-<label for="">Last Name:</label>
-<input type="text" name="lastname" value="<?php echo $row['Last_name'] ?>" >
+<label class="form-label" for="">Last Name:</label>
+<input class="form-control" type="text" name="lastname" value="<?php echo $row['Last_name'] ?>" >
   </div>
-
+<br>
   <div class="age">
-    <label for="">Age:</label>
-    <input type="number" name="age" value="<?php echo $row['age'] ?>">
+    <label class="form-label" for="">Age:</label>
+    <input class="form-control" type="number" name="age" value="<?php echo $row['age'] ?>">
   </div>
-
+<br>
   <div class="genders">
-    <label for="">Gender:</label>
+    <label class="form-label" for="">Gender:</label>
    <select name="gender" id="genderid" >
     <option value="male">Male</option>
     <option value="female">Female</option>
     <option value="email">Email</option>
    </select>
   </div>
+   </div>
 
+<div class="con2">
 <div class="username">
-    <label for="">Username:</label>
-    <input type="text" name="username" placeholder="user@example.com" value="<?php echo $row['username'] ?>">
+    <label class="form-label" for="">Username:</label>
+    <input class="form-control" type="text" name="username" placeholder="user@example.com" value="<?php echo $row['username'] ?>">
 </div>
-
+<br>
 <div class="password">
-    <label for="">Password:</label>
-    <input type="text" name="password" value="<?php echo $row['password'] ?>" >
+    <label class="form-label" for="">Password:</label>
+    <input class="form-control" type="text" name="password" value="<?php echo $row['password'] ?>" >
 </div>
-
+<br>
 <div class="cpassword">
-    <label for="">Confirm Password:</label>
-    <input type="text" name="cpass" >
+    <label class="form-label" for="">Confirm Password:</label>
+    <input  class="form-control" type="text" name="cpass" >
 </div>
-
+<br>
 <div class="img">
-    <label for="">profile: </label>
+    <label class="form-label" for="">profile: </label>
     <input type="file" name="file">
-    <input type="hidden" name="date_uploaded" value="<?php echo $row['date_uploaded'] ?>">
+    <input type="hidden" class="form-control" name="date_uploaded" value="<?php echo $row['date_uploaded'] ?>">
     <input type="hidden" name="current_file" value="<?php echo $row['image'] ?>">
 </div>
-
-<div class="btn">
-<input type="submit" value="Create" name="update">
 </div>
+  </div>
+<br>
+
+
+<input type="submit" value="Create" name="update" style="width: 60%;">
+</center>
+
 
 
     </form>
